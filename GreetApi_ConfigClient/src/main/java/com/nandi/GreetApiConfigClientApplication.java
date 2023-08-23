e504f424122ca410731fc80cd1a324e3578c223d
sangamesh <nandikolsangamesh27@gmail.com>
2023-08-23 13:09:57 +0530

    adding cloud config server

---
+++ GreetApi_ConfigClient/src/main/java/com/nandi/GreetApiConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.nandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetApiConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetApiConfigClientApplication.class, args);
+	}
+
+}
---
+++ WelcomeApi_ConfigClient/src/main/java/com/nandi/WelcomeApiConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.nandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeApiConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApiConfigClientApplication.class, args);
+	}
+
+}
